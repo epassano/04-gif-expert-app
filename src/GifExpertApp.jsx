d2fa4eb11937e5bb561c7713ef3344bc2f9e4eae
epassano <eapch21@gmail.com>
2024-11-13 18:36:26 -0500

    75. GifExpertApp - Component

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -2,7 +2,12 @@
 export const GifExpertApp = () => {
   return (
     <>
+    {/*Titulo */}
     <h1>GifExpertApp</h1>
+    {/*Input */}
+    {/*Listado de Gif */}
+    {/*Gif item */}
+
     </>
   )
 }
