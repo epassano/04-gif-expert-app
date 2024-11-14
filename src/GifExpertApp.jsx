786b1eb4431855bd9ec571c76cd5099f47a17d59
epassano <eapch21@gmail.com>
2024-11-14 17:42:20 -0500

    82. GifGrid - Nuevo componente

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
   
@@ -12,19 +13,22 @@ export const GifExpertApp = () => {
     }
   return (
     <>
-    {/*Titulo */}
+    
     <h1>GifExpertApp</h1>
     <AddCategory 
       onNewCategory={ (event) => onAddCategory(event)}
     />
-    {/*Listado de Gif */}
-    <button onClick={onAddCategory}>Agregar</button>
-    <ol>
-     {categories.map(category=>{
-        return <li key={category}>{category}</li>
-     })}
-    </ol>
-    {/*Gif item */}
+
+   
+     {
+      categories.map((category)=>
+       (
+        <GifGrid key={category} 
+                category={category}/>
+        )
+     )}
+   
+ 
 
     </>
   )
