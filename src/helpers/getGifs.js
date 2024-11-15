49b86d46c5385819dd827bbd56e0d40ff29d4cec
epassano <eapch21@gmail.com>
2024-11-15 13:38:00 -0500

    84. useEffect

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs=async(category)=>{
 
-    const url=`https://api.giphy.com/v1/gifs/trending?api_key=KLChVo98QDXQw5jX0T9K2cz9XNAVjmnr&q=${ category}&limit=20`
+    const url=`https://api.giphy.com/v1/gifs/trending?api_key=KLChVo98QDXQw5jX0T9K2cz9XNAVjmnr&q=${ category}&limit=10`
 
     const resp= await fetch( url );
 
